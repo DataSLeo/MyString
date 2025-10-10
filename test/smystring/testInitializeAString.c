56d0bd25ff43e9b8427f59cb37f1c6369b2f4b32
Leonardo Alves <datascileo@gmail.com>
2025-10-10 16:34:05 -0300

    refactor (testInitializeAString.c): removed test property 'initialized'

--- test/smystring/testInitializeAString.c
+++ test/smystring/testInitializeAString.c
@@ -7,21 +7,15 @@
 SCENARIES:
 
 1. Initialize a String and Verify it's properties (size = 0, id_error = 0).
-2. Initialize a String initialized.
 
 */
 
 
 void testInitializeAStringNewString();
-void testInitializeAStringAlreadyInitialized();
-
 
 int main () {
     testInitializeAStringNewString();
     printf("testInitializeAStringNewString passed\n");
-
-    testInitializeAStringAlreadyInitialized();
-    printf("testInitializeAStringAlreadyInitialized passed\n");
 
     return 0;
 }
@@ -34,16 +28,5 @@ void testInitializeAStringNewString() {
 
     cr_assert_eq(string.size, 0);     
     cr_assert_eq(string.id_error, MYSTRING_NONE);
-
-}
-
-void testInitializeAStringAlreadyInitialized() {
-
-    struct MyString string;
-    init_string(&string);
-
-    init_string(&string);
-
-    cr_assert_eq(string.id_error, MYSTRING_STRING_ALREADY_INITIALIZED);
 
 }
