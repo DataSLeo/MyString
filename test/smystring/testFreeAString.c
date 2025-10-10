126be97401ef6038824a146b529c891f053c2b37
Leonardo Alves <datascileo@gmail.com>
2025-10-10 16:33:20 -0300

    refactor (testFreeAString.c): add a new test

--- test/smystring/testFreeAString.c
+++ test/smystring/testFreeAString.c
@@ -10,13 +10,14 @@ SCENARIES:
 1. After use del_string the size property must be Zero 
 2. After use del_string the phrase property must be Null
 3. After use del_string the id_error must be MYSTRING_NONE
-
+4. Try use del_string again makes a id_error 
 */
 
 
 void testFreeAStringSizeIsZero();
 void testFreeAStringPhraseIsNull();
 void testFreeAStringErrorNone();
+void testFreeAStringAgainMakesAnError();
 
 
 int main () {
@@ -28,6 +29,9 @@ int main () {
 
     testFreeAStringErrorNone();
     printf("testFreeAStringErrorNone passed\n");
+
+    testFreeAStringAgainMakesAnError();
+    printf("testFreeAStringAgainMakesAnError passed\n");
 
     return 0;
 }
@@ -42,6 +46,8 @@ void testFreeAStringSizeIsZero() {
 
     cr_assert_eq(string.size, 0);
 
+    return;
+
 }
 
 void testFreeAStringPhraseIsNull() {
@@ -52,6 +58,7 @@ void testFreeAStringPhraseIsNull() {
 
     cr_assert(string.phrase == NULL);
 
+    return;
 }
 
 void testFreeAStringErrorNone() {
@@ -60,6 +67,22 @@ void testFreeAStringErrorNone() {
 
     del_string(&string);
 
-    cr_assert(string.id_error == MYSTRING_NONE);
+    cr_assert_eq(string.id_error, MYSTRING_NONE);
+
+    return;
+
+}
+
+void testFreeAStringAgainMakesAnError() {
+    struct MyString string;
+    init_string(&string);
+
+    del_string(&string);
+
+    del_string(&string);
+
+    cr_assert_eq(string.id_error, MYSTRING_STRING_ALREADY_FREE);
+
+    return;
 
 }
