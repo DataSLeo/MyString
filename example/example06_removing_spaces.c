1743f23ac4b8c52f2a31bee3c802faecdf11c24a
Leonardo Alves <datascileo@gmail.com>
2025-10-11 21:13:12 -0300

    docs: add example06_removing_spaces to demonstrate remasp_string function

---
+++ example/example06_removing_spaces.c
@@ -0,0 +1,73 @@
+/**
+ * Example 06 - Removing all spaces in a MyString object.
+ * Demonstrates how to remove all spaces.
+ *
+ * To compile:
+ * gcc example/example06_removing_spaces.c -Iinclude -Lbuild -lmystring -o example/example06
+ */
+
+
+#include <stdio.h>
+#include "mystring.h"
+
+
+int main () {
+ 
+    /** 
+     * The Struct of String, if you want you can use the typedef to remove keyword "struct"
+     * 
+     * Initializing two MyString objects
+     */
+    struct MyString example;
+
+
+    // Initializing a String with a phrase "C is the best language"
+    enum ErrorMyString e = initwp_string(&example, "C is the best language"); 
+
+    if(e) {
+        printf("Initialization failed with id error [%d]\n", e);
+
+        /**
+         * You can get erro too using the property id_error from yout MyString object. 
+         */
+
+    }
+
+    // Printing the string (example) and the length
+    printf("%s (%d)\n", example.phrase, example.length);
+
+
+    // Removing the spaces
+    e = remasp_string(&example);
+
+    if(e) {
+        printf("Remove spaces failed with id error [%d]\n", e);
+
+        /**
+         * You can get erro too using the property id_error from yout MyString object. 
+         */
+
+    }    
+
+
+    // Printing the result string and the length
+    printf("%s (%d)\n", example.phrase, example.length);
+
+
+    // Free the String
+    e = del_string(&example);
+
+    if(e) {
+        printf("Deletation failed with id error [%d]\n", e);
+
+        /**
+         * You can get erro too using the property id_error from yout MyString object. 
+         */
+
+    }
+
+
+    printf("\n==> Everything worked perfectly. Goodbye! <==\n");
+
+    return 0;
+}
