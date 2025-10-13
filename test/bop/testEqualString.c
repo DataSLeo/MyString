cdb1779f981a58574b71008e0e5aab2dcbc193bc
Leonardo Alves <datascileo@gmail.com>
2025-10-13 20:17:47 -0300

    refactor: testEqualString in criterion now (closes #04)

--- test/bop/testEqualString.c
+++ test/bop/testEqualString.c
@@ -1,75 +1,62 @@
-#include <stdio.h>
 #include <criterion/criterion.h>
-
 #include "mystring.h"
 
 /* 
-SCENARIES:
+ * SCENARIOS:
+ *
+ * 1. One of Strings compared has differents sizes
+ * 2. The Strings are equals
+ * 3. Two String has filled, but are differents
+ */
 
-1. One of Strings compared has differents sizes
-2. The Strings are equals
-3. Two String has filled, but are differents
 
-*/
+Test(TestEqualString, OneOfStringsHasDifferentsSizes) {
 
+    struct MyString object1;
+    struct MyString object2;
 
-void testEqualStringOneOfStringsHasDifferentsSizes();
-void testEqualStringAllIsEquals();
-void testEqualStringDifferentBetween();
+    initwp_string(&object1, (char*)"Kent Beck");
+    initwp_string(&object2, (char*)"Linus Torvalds");
 
-int main() {
+    int result = equals_string(object1, object2);
 
-    testEqualStringOneOfStringsHasDifferentsSizes();
-    printf("testEqualStringOneOfStringsHasDifferentsSizes passed\n");
+    cr_assert_eq(result, 0, "Expected false (0), got true (%d)", result);
 
-    testEqualStringAllIsEquals();
-    printf("testEqualStringAllIsEquals passed\n");
-
-    testEqualStringDifferentBetween();
-    printf("testEqualStringDifferentBetween passed\n");
-
-    return 0;
-}
-
-void testEqualStringOneOfStringsHasDifferentsSizes() {
-
-    struct MyString string1;
-    initwp_string(&string1, (char*)"Kent Beck");
-
-    struct MyString string2;
-    initwp_string(&string2, (char*)"Linux Torvalds");
-
-    int result = equals_string(string1, string2);
-
-    cr_assert_eq(0, result);
-
-    return;
-}
-
-void testEqualStringAllIsEquals() {
-
-    struct MyString string1;
-    initwp_string(&string1, (char*)"C is best");
-    
-    struct MyString string2;
-    initwp_string(&string2, (char*)"C is best");
-
-    int result = equals_string(string1, string2);
-
-    cr_assert_eq(1, result);
+    del_string(&object1);
+    del_string(&object2);
 
 }
 
-void testEqualStringDifferentBetween() {
+Test(TestEqualString, AllIsEquals) {
 
-    struct MyString string1;
-    initwp_string(&string1, (char*)"Coffee");
+    struct MyString object1;
+    struct MyString object2;
 
-    struct MyString string2;
-    initwp_string(&string2, (char*)"Milk");
+    initwp_string(&object1, (char*)"C is best");
+    initwp_string(&object2, (char*)"C is best");
 
-    int result = equals_string(string1, string2);
+    int result = equals_string(object1, object2);
 
-    cr_assert_eq(0, result);
+    cr_assert_eq(result, 1, "Expected true (1), got false (%d)", result);
 
-} 
+    del_string(&object1);
+    del_string(&object2);
+
+}
+
+Test(TestEqualString, DifferentBetween) {
+
+    struct MyString object1;
+    struct MyString object2;
+
+    initwp_string(&object1, (char*)"Coffee");    
+    initwp_string(&object2, (char*)"Milk");
+
+    int result = equals_string(object1, object2);
+
+    cr_assert_eq(result, 0, "Expected 0, got %d", result);
+
+    del_string(&object1);
+    del_string(&object2);
+
+}
